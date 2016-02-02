de9496a8408bfa21ef6807ad5d1dbbfd7d0635ba
Philippe Vienne <philippe@vienne.me>
2016-02-02 14:20:45 +0100

    Initial commit

---
+++ src/fr/insa/lyon/tc/elp/ArgumentsApp.java
@@ -0,0 +1,15 @@
+package fr.insa.lyon.tc.elp;
+
+public class ArgumentsApp {
+    public static void main(String... args){
+        System.out.println("Paramètres de l'application par un foreach :");
+        for(String param:args){
+            System.out.println(param);
+        }
+        System.out.println("\nParamètres de l'application par un for avec iteration :");
+        for (int i = 0, argsLength = args.length; i < argsLength; i++) {
+            String param = args[i];
+            System.out.println("N°"+i+": "+param);
+        }
+    }
+}
---
+++ src/fr/insa/lyon/tc/elp/HelloWorldApp.java
@@ -0,0 +1,7 @@
+package fr.insa.lyon.tc.elp;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
