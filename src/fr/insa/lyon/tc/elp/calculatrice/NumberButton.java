570abceb876d3eaaa2dc7a06b701ba515e77512c
Philippe Vienne <philippe@vienne.me>
2016-03-16 10:53:14 +0100

    Add number button

---
+++ src/fr/insa/lyon/tc/elp/calculatrice/NumberButton.java
@@ -0,0 +1,27 @@
+package fr.insa.lyon.tc.elp.calculatrice;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ * Number button
+ */
+public class NumberButton extends JButton implements ActionListener {
+
+    private final int value;
+    private final CalculatriceFrame frame;
+
+    public NumberButton(CalculatriceFrame frame, int i){
+        super(Integer.toString(i));
+        this.value = i;
+        this.frame = frame;
+        addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        frame.setCurrentValue(frame.getCurrentValue()*10+value);
+    }
+}
